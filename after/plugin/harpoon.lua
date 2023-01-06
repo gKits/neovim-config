57325d4d7c2827e96101927ec5615a8631a78fde
Georgios <gkitsikoudis@gmx.de>
2023-01-06 11:00:41 +0100

    Harpoon remaps Removed <C-w> remap for terminal Now uses <C-[g,h,j,k,l]> for first 5 buffers in harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,14 +5,9 @@ local term = require("harpoon.term")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
--- Keymaps for first seven buffers
-vim.keymap.set("n", "<C-y>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-x>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-c>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-v>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-b>", function() ui.nav_file(5) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(6) end)
-vim.keymap.set("n", "<C-m>", function() ui.nav_file(7) end)
-
--- Keymaps for terminal
-vim.keymap.set("n", "<C-w>" , function() term.gotoTerminal(1) end)
+-- Keymaps for first five buffers
+vim.keymap.set("n", "<C-g>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(5) end)
